39e72588f247334a4807a7c33b0011aea909533c
wonho-seo <63235720+wonho-seo@users.noreply.github.com>
2020-05-15 17:03:46 +0900

    Add files via upload

--- 2. Data preprocessing/test1.py
+++ 2. Data preprocessing/test1.py
@@ -1,10 +1,7 @@
-
 import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import os
 import cv2
-import MNIST_data
+import pandas as pd
 import glob
 
 
@@ -37,97 +34,80 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
 
 
 kernel_size = 5  # 가우시안
-k = 0
+
 rho = 5
 theta = np.pi / 180
-threshold = 220
-min_line_len = 150
-max_line_gap = 150
-
-imges = [cv2.imread(pro) for pro in glob.glob("D:\pro1\*.jpg")]
-
-for i in range(0, len(imges)):
-
-    img = imges[i]
-
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  # gray
-
-    blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
-    for y in range(0,10):
-        blur_gray = cv2.GaussianBlur(blur_gray , (kernel_size, kernel_size), 10)
-
-    low_threshold = 10
-    high_threshold = 20
-    edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
-
-    if len(img.shape) > 2:
-        channel_count = img.shape[2]
-        ignore_mask_color = (255,)
-    else:
-        ignore_mask_color = 255
-
-    vertices1 = np.array([[(500, 1000),
-                          (730, 750),
-                          (840, 750),
-                          (725, 1000)]], dtype=np.int32)
-    vertices2 = np.array([[(725, 1000),
-                          (840, 700),
-                          (950, 700),
-                          (950, 1000)]], dtype=np.int32)
-    vertices3 = np.array([[(950, 1000),
-                          (950, 700),
-                          (1060, 700),
-                          (1175, 1000)]], dtype=np.int32)
-    vertices4 = np.array([[(1175, 1000),
-                          (1060, 700),
-                          (1180, 700),
-                          (1400, 1000)]], dtype=np.int32)
+threshold = 60
+min_line_len = 50
+max_line_gap = 50
+low_threshold = 10
+high_threshold = 20
+Loadplace="D:\pro8/"
+dir=os.listdir('D:\pro8/')
+Saveplace="D:\Smaple 10/"
+CSVfile = '20200513134840.csv'
 
 
-    mask1 = region_of_interest(edges, vertices1)
-    mask2 = region_of_interest(edges, vertices2)
-    mask3 = region_of_interest(edges, vertices3)
-    mask4 = region_of_interest(edges, vertices4)
+csv_svdata=pd.DataFrame()
+for filenum in range(0,len(dir)):
+    k=0
+    os.makedirs(Saveplace+dir[filenum]+'/')
+    imges = [cv2.imread(pro) for pro in glob.glob(Loadplace+ dir[filenum] + "\*.jpg")]
+    csv_data = pd.read_csv(Loadplace+ dir[filenum] +'/' + dir[filenum] + '.csv')
+    csv_svdata = pd.DataFrame()
+    for i in range(0, len(imges)):
 
-    try:
-        lines1 = hough_lines(mask1, rho, theta, threshold, min_line_len, max_line_gap)
-        li1 = True
-    except:
+        img = imges[i]
 
-        li1 = False
-    try:
-        lines2 = hough_lines(mask2, rho, theta, threshold, min_line_len, max_line_gap)
-        li2 = True
-    except:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  # gray
 
-        li2 = False
-    try:
-        lines3 = hough_lines(mask3, rho, theta, threshold, min_line_len, max_line_gap)
-        li3 = True
-    except:
+        blur_gray = cv2.GaussianBlur(gray, (kernel_size, kernel_size), 0)
+        for y in range(0, 10):
+            blur_gray = cv2.GaussianBlur(blur_gray, (kernel_size, kernel_size), 10)
 
-        li3 = False
-    try:
-        lines4 = hough_lines(mask4, rho, theta, threshold, min_line_len, max_line_gap)
-        li4 = True
-    except:
+        edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
-        li4 = False
+        vertices1 = np.array([[(73, 70),
+                               (83, 0),
+                               (95, 0),
+                               (95, 70)]], dtype=np.int32)
+        vertices2 = np.array([[(95, 70),
+                               (95, 0),
+                               (110, 0),
+                               (120, 70)]], dtype=np.int32)
 
+        mask1 = region_of_interest(edges, vertices1)
+        mask2 = region_of_interest(edges, vertices2)
 
+        try:
+            lines1 = hough_lines(mask1, rho, theta, threshold, min_line_len, max_line_gap)
+            li1 = True
+        except:
 
+            li1 = False
+        try:
+            lines2 = hough_lines(mask2, rho, theta, threshold, min_line_len, max_line_gap)
+            li2 = True
+        except:
 
+            li2 = False
 
-    if ((li1==True)|(li2==True)|(li3==True)|(li4==True)):
-        if ((i - k) > 3):
-            for t in range(k, (i - 3)):
-                cv2.imshow("", imges[t])
-                cv2.imwrite('D:\sample2\y' + str(t) + '.jpg', imges[t])
-            k = i
-        else:
-            k = i
-    if(i==(len(imges)-1)):
-        for t in range(k,i):
-            cv2.imwrite('D:\sample2\y' + str(t) + '.jpg', imges[t])
+        if ((li1 == True) | (li2 == True)):
+            if ((i - k) > 3):
+                for t in range((k + 1), (i - 2)):
+                    cv2.imwrite(Saveplace + dir[filenum] + '/' + str(csv_data.ix[t - 1][1]), imges[t])
+                    csv_svdata = csv_svdata.append(csv_data.iloc[t])
+                k = i
+            else:
+                k = i
+        if (i == (len(imges) - 1)):
+            for t in range((k + 1), i):
+                cv2.imwrite(Saveplace + dir[filenum] + '/' + str(csv_data.ix[t][1]), imges[t])
+                csv_svdata = csv_svdata.append(csv_data.iloc[t])
+    csv_svdata = csv_svdata.loc[:, ["file_name", "break", "accel", "wheel"]]
+
+    csv_svdata.to_csv(Saveplace + dir[filenum] + '/' + dir[filenum] + '.csv')
+
+print(len(dir))
 cv2.waitKey(0)
 cv2.destroyWindow()
